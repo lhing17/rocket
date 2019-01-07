d2de0985a74eda2081a5bcd2484dde7f6b95bcca
lhing17 <lhing17@163.com>
2019-01-07 21:26:48 +0800

    角色和权限相关代码。

--- rocket-admin/src/main/java/cn/gsein/system/mapper/PermissionMapper.java
+++ rocket-admin/src/main/java/cn/gsein/system/mapper/PermissionMapper.java
@@ -3,6 +3,9 @@ package cn.gsein.system.mapper;
 import cn.gsein.system.entity.Permission;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * 权限相关的持久层
  *
@@ -11,4 +14,19 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PermissionMapper extends BaseMapper<Permission> {
+    /**
+     * 获取一个角色能访问的所有权限
+     *
+     * @param roleId 角色ID
+     * @return 角色能访问的所有权限
+     */
+    List<Permission> getPermissionsByRoleId(Integer roleId);
+
+    /**
+     * 获取多个角色能访问的全部权限集合
+     *
+     * @param roleIds 角色ID列表
+     * @return 多个角色能访问的所有权限集合
+     */
+    Set<Permission> getPermissionsByRoleIds(List<Integer> roleIds);
 }
---
+++ rocket-admin/src/main/java/cn/gsein/system/mapper/RolePermissionMapper.java
@@ -0,0 +1,22 @@
+package cn.gsein.system.mapper;
+
+import cn.gsein.system.entity.RolePermission;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 角色-权限关系持久层
+ *
+ * @author G.Seinfeld
+ * @date 2019/1/3
+ */
+@Repository
+public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+    /**
+     * 根据角色ID删除所有角色-权限关系
+     *
+     * @param roleId 角色ID
+     * @return 删除成功返回一个正整数，否则返回0
+     */
+    int deleteByRoleId(Integer roleId);
+}
